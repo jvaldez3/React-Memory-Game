2476a6e6f9706ab49614dd2db7fb10b2e4e6b7a1
jval <joshvaldez3320@gmail.com>
2019-06-19 16:10:37 -0600

    Good commit  point

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Card from "./components/GameCards";
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import characters from ".characters.json";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      characters: characters,
+      topScore: 0,
+      currentScore: 0
+    };
+    this.checkIfClicked = this.checkIfClicked.bind(this);
+  }
+
+  checkIfClicked(id) {
+    let clickedCharacter = this.state.characters.filter(
+      char => char.id === id
+    )[0];
+    let characterCopy = this.state.characters.slice().sort(function(a, b) {
+      return 0.5 - Math.random();
+    });
+
+    if (!clickedCharacter.clicked) {
+      clickedCharacter.clicked = true;
+      characterCopy[
+        characterCopy.findIndex(char => char.id === id)
+      ] = clickedCharacter;
+
+      this.setState({
+        characters: characterCopy,
+        currentScore: this.state.currentScore + 1,
+        topScore:
+          this.state.currentScore + 1 > this.state.topScore
+            ? this.state.currentScore + 1
+            : this.state.topScore
+      });
+    } else {
+      let resetCharacterCopy = characterCopy.map(char => {
+        return {
+          id: char.id,
+          image: char.image,
+          clicked: false
+        };
+      });
+      this.setState({
+        characters: resetCharacterCopy,
+        currentScore: 0
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <Header
+          currentScore={this.state.currentScore}
+          topScore={this.state.topScore}
+        />
+        <Wrapper>
+          {/* <Title>Character List</Title> */}
+          {this.state.characters.map(characters => (
+            <Card
+              checkIfClicked={this.checkIfClicked}
+              id={characters.id}
+              key={characters.id}
+              image={characters.image}
+            />
+          ))}
+        </Wrapper>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ react-app/src/components/GameCards/Card.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ".style.css";
+
+// set properties for the image and the onClick event to see if the card has been clicked or not already
+const gameCards = props => (
+  <div className="card">
+    <div className="img-container">
+      <img
+        alt={"This is image " + props.id}
+        src={props.image}
+        onClick={() => props.checkIfClicked(props.id)}
+      />
+    </div>
+  </div>
+);
+
+export default gameCards;
