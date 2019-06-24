f93fb7f57c1a464d7d48269bca3b4cf84236e035
jval <joshvaldez3320@gmail.com>
2019-06-24 11:18:38 -1000

    Updates

---
+++ precache-manifest.97116dc1a9ab78dfc1eda1787daf5988.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "073e3b299f3a384b0f8709a5b44cb109",
+    "url": "/React-Memory-Game/index.html"
+  },
+  {
+    "revision": "467be1763b1f1162d31b",
+    "url": "/React-Memory-Game/static/css/main.767cd378.chunk.css"
+  },
+  {
+    "revision": "9b3a5007987034dff77a",
+    "url": "/React-Memory-Game/static/js/2.652dea17.chunk.js"
+  },
+  {
+    "revision": "467be1763b1f1162d31b",
+    "url": "/React-Memory-Game/static/js/main.a7017487.chunk.js"
+  },
+  {
+    "revision": "146a3181e76c06b70ec8",
+    "url": "/React-Memory-Game/static/js/runtime~main.39b1e4df.js"
+  }
+]);
